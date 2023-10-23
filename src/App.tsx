23859f6dfc0c16df1f70663a51fd579ccc9d9225
olvalaskevich <olvol269@gmail.com>
2023-10-23 09:23:37 +0300

    change code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import {Rating} from "./components/Rating/Rating";
+
+import {Accordion} from "./components/Accordion/Accordion";
+import {Button} from "./Button";
+
 function App() {
+    let [open, setOpen]=useState(false)
+
+    let [ratingValue,setRatingValue]=useState(0)
+
+    let [on, setOn]=useState(false)
+
+    const openAccordion=()=>{
+        setOpen(!open)
+    }
+
+    function selectedStar(value:number){
+        setRatingValue(value)
+    }
+
+    function tumbler(){
+        setOn(!on)
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+        <Accordion title={"Menu"} open={open} openAccordion={openAccordion}/>
+        <Rating value={ratingValue} selectedStar={selectedStar}/>
+        {/*<OnOff on={on} tumbler={tumbler}/>*/}
+        <Button title={'On'}/>
     </div>
+
   );
 }
+
+type AppTitlePropsType={
+    title:string
+}
+
+function AppTitle(props:AppTitlePropsType) {
+    return (
+      <div>{props.title}</div>
+    )
+}
+
+
 
 export default App;
